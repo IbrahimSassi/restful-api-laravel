7dddd42696845d7288a0399e150e7ff74d928b18
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-09-06 23:25:30 +0100

    Authentification - Creating the Register Endpoint

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -62,10 +64,47 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+//        return response()->json([
+//            'auth' => 'ok'
+//        ], 200);
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
     }
+
+
+    //Added By Me
+    public function register(Request $request)
+    {
+        // Here the request is validated. The validator method is located
+        // inside the RegisterController, and makes sure the name, email
+        // password and password_confirmation fields are required.
+        $this->validator($request->all())->validate();
+
+        // A Registered event is created and will trigger any relevant
+        // observers, such as sending a confirmation email or any
+        // code that needs to be run as soon as the user is created.
+        event(new Registered($user = $this->create($request->all())));
+
+        // After the user is created, he's logged in.
+        $this->guard()->login($user);
+
+        // And finally this is the hook that we want. If there is no
+        // registered() method or it returns null, redirect him to
+        // some other URL. In our case, we just need to implement
+        // that method to return the correct response.
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
+    }
+
+    protected function registered(Request $request, $user)
+    {
+        $user->generateToken();
+
+        return response()->json(['data' => $user->toArray()], 201);
+    }
+
 }
