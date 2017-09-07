771241cba97f8cdea778d94d5a34e4d610768c20
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-09-07 20:24:57 +0100

    Logging Out

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -65,10 +65,12 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
+        return response()->json(['error' => 'Unauthenticated'], 401);
 
-        return redirect()->guest(route('login'));
+//        if ($request->expectsJson()) {
+//            return response()->json(['error' => 'Unauthenticated.'], 401);
+//        }
+//
+//        return redirect()->guest(route('login'));
     }
 }
